3867f0debe1b9a1916e2788c611ae126134d5b25
Rafael Luz <rafa_luz_araujo@hotmail.com>
2022-06-13 14:17:37 -0300

    crud completo

--- petcapau2/petshop/urls.py
+++ petcapau2/petshop/urls.py
@@ -5,6 +5,9 @@ app_name = "petshop"
 
 urlpatterns = [
     path('olamundo', views.HelloWorld),
-    path('pet/list', views.ListPet, name='list_pet'),
-    path('pet/details/<int:pk>', views.PetDetail, name='pet_detail'),
+    path('pet/list', views.ListPet.as_view(), name='pet_list'),
+    path('pet/details/<int:pk>', views.PetDetail.as_view(), name='pet_detail'),
+    path('pet/create', views.PetCreate.as_view(), name='pet_create'),
+    path('pet/update/<int:pk>', views.PetUpdate.as_view(), name='pet_update'),
+    path('pet/delete/<int:pk>', views.PetDelete.as_view(), name='pet_delete'),
 ]
--- petcapau2/petshop/views.py
+++ petcapau2/petshop/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Pet 
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
 
 # Create your views here.
 def HelloWorld(request):
@@ -7,11 +10,34 @@ def HelloWorld(request):
     alunos = ['alex', 'jean', 'daniel', 'karielly', 'wanda', 'shara']
     return render(request, 'pet/hello_world.html', {'mensagem':mensagem, 'alunos':alunos})
 
+class ListPet(ListView):
+    model = Pet
+    template_name = 'pet/pet_list.html'
 
-def ListPet(request):
-    pets = Pet.objects.all()
-    return render(request, 'pet/pet_list.html',{'pets': pets})
+class PetDetail(DetailView):
+    model = Pet
+    template_name = 'pet/pet_detail.html' 
 
-def PetDetail(request, pk):
-    pet = Pet.objects.get(pk=pk)
-    return render(request, 'pet/pet_detail.html',{'pet': pet})
+class PetCreate(CreateView):
+    model = Pet
+    template_name = 'pet/pet_create.html'
+    # fields = ['name','species','breed','sex','birth_date','hair','tatoo']
+    fields = '__all__'
+    # success_url = reverse_lazy('petshop:pet_list')
+
+    def get_success_url(self):
+        return reverse('petshop:pet_list')
+
+
+class PetUpdate(UpdateView):
+    model = Pet
+    template_name = 'pet/pet_create.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse('petshop:pet_list')
+
+class PetDelete(DeleteView):
+    model = Pet
+    template_name = 'pet/pet_confirm_delete.html'
+    success_url = reverse_lazy('petshop:pet_list')
